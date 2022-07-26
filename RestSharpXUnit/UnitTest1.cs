096b5b073db8043c87ff7553b9a0fc34dc7b45b2
mdarda <maciejdarda@gmail.com>
2022-07-26 19:31:31 +0200

    Add RestSharp nuget

--- RestSharpXUnit/UnitTest1.cs
+++ RestSharpXUnit/UnitTest1.cs
@@ -1,11 +1,20 @@
+using Xunit.Abstractions;
+
 namespace RestSharpXUnit
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void Test1()
         {
-
+            testOutputHelper.WriteLine("Executing 1st test");
         }
     }
 }
